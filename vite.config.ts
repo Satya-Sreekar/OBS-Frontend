b642a3a2bebd5eff30e39e27320854315dc8e0ec
sunilimpereal <sunilhari496@gmail.com>
2025-03-31 13:27:07 +0530

    updated vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    allowedHosts: ['ec2-34-195-0-32.compute-1.amazonaws.com']
   }
-})
+});
